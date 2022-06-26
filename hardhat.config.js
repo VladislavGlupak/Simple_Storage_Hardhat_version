1a44e7cc76d2b6e7af731af22824c39e89d343bd
Vlad Glupak <vglupak@Vlads-MBP.hsd1.ca.comcast.net>
2022-06-25 18:44:13 -0700

    added ganache

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,6 +29,10 @@ module.exports = {
             url: "http://127.0.0.1:8545",
             chainId: 31337,
         },
+        ganache: {
+            url: "http://127.0.0.1:8545",
+            chainId: 1337,
+        },
     },
     solidity: "0.8.7",
     etherscan: {
